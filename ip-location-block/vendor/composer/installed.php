dcbf600c500c9e2802f92bcad1172353c333f5d5
Sean Murphy <sean@iamseanmurphy.com>
2026-08-24 13:12:40 +0000

    1.4.0

---
+++ ip-location-block/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iplocationblock/ip-location-block',
+        'pretty_version' => 'v1.4.0',
+        'version' => '1.4.0.0',
+        'reference' => '0aaff54eee5040490c468238efa7cc8359135a1b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'iplocationblock/ip-location-block' => array(
+            'pretty_version' => 'v1.4.0',
+            'version' => '1.4.0.0',
+            'reference' => '0aaff54eee5040490c468238efa7cc8359135a1b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
